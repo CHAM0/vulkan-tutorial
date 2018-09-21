f4f9541a43c8e4e5059035a56a48c6956bfd2bd4
Jarod <jarod8405@gmail.com>
2018-09-22 00:23:01 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
-#[macro_use]
-extern crate vulkano;
+#[macro_use] extern crate vulkano;
+#[macro_use] extern crate vulkano_shader_derive;
 extern crate vulkano_win;
 extern crate winit;
+extern crate image;            
 
-
-use vulkano::instance::Instance;
-use vulkano::instance::InstanceExtensions;
-use vulkano::instance::PhysicalDevice;
-use vulkano::instance::Features;
-use vulkano::device::Device;
-use vulkano::device::DeviceExtensions;
-use vulkano::buffer::BufferUsage;
-use vulkano::buffer::CpuAccessibleBuffer;
+use vulkano::instance::{ Instance, InstanceExtensions, PhysicalDevice, Features };
+use vulkano::device::{ Device, DeviceExtensions };
+use vulkano::buffer::{ BufferUsage, CpuAccessibleBuffer };
+use vulkano::pipeline::ComputePipeline;
+use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+use vulkano::command_buffer::{ AutoCommandBufferBuilder, CommandBuffer };
+use vulkano::sync::GpuFuture;
+use vulkano::format::{ Format, ClearValue };
+use vulkano::image::{ Dimensions, StorageImage };
 
 use vulkano_win::VkSurfaceBuild;
 
 use winit::WindowBuilder;
 use winit::EventsLoop;
 
+use std::sync::Arc;
 
+use image::{ ImageBuffer, Rgba };
+
+
+mod cs {
+    #[derive(VulkanoShader)]
+    #[ty = "compute"]
+    #[path = "src/shaders/operation.glsl"]
+    #[allow(dead_code)]
+
+    struct Dummy;
+}
 
 fn main() {
     // Create an instances who initializes everything 
@@ -58,11 +71,70 @@ fn main() {
             };
 
     // Get our queue 
-    let queue = queues.next();
+    let queue = queues.next().expect("Failed to get our queue");
 
 
-    let data = 12;
-    let buffer = CpuAccessibleBuffer::from_data(device.clone(), BufferUsage::all(), data);
+    // Introduction to compute operations
+    let data_iter = 0 .. 65536;
+    let data_buffer = match CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), data_iter) {
+        Ok(i) => i,
+        Err(err) => panic!("Failed to create a buffer: {:?}", err)
+    };
+
+    // Create shader module
+    let shader = match cs::Shader::load(device.clone()) {
+        Ok(i) => i,
+        Err(err) => panic!("Failed to create a shader module: {:?}", err),
+    };
+
+    // Create a compute pipeline
+    let compute_pipeline = Arc::new(ComputePipeline::new(device.clone(), &shader.main_entry_point(), &())
+        .expect("failed to create compute pipeline"));
+
+    // Create a descriptor set
+    let set = Arc::new(PersistentDescriptorSet::start(compute_pipeline.clone(), 0)
+        .add_buffer(data_buffer.clone()).unwrap()
+        .build().unwrap()
+    );
+
+    // Create a command buffer 
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
+        .dispatch([1024,1,1], compute_pipeline.clone(), set.clone(), ()).unwrap()
+        .build().unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
+
+    let content = data_buffer.read().unwrap();
+        for (n, val) in content.iter().enumerate() {
+            assert_eq!(*val, n as u32 * 12);
+    }
+    println!("Everything succeeded!");
+
+
+
+    // Image creation
+    let image = StorageImage::new(device.clone(), Dimensions::Dim2d {width: 1024, height: 1024},
+        Format::R8G8B8A8Unorm, Some(queue.family())).unwrap();
+
+    let buf = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+        (0..1024 * 1024 * 4).map(|_| 0u8))
+        .expect("Failed to create buffer");
+    
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
+        .clear_color_image(image.clone(), ClearValue::Float([0.3, 0.3, 0.3, 1.0])).unwrap()
+        .copy_image_to_buffer(image.clone(), buf.clone()).unwrap()
+        .build().unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
+
+    let buffer_content = buf.read().unwrap();
+    let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+    image.save("image.png").unwrap();
 
     // Create a window
     let mut events_loop = EventsLoop::new();
